cb45c405338dd1c733ebceecc447f1f8bebf495a
Vernichenko <oleksandrvernichenko@gmail.com>
2023-10-23 12:37:18 +0300

    feat: add error

--- fe/app/vite.config.ts
+++ fe/app/vite.config.ts
@@ -1,9 +1,13 @@
 /* eslint-disable import/no-default-export */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  // @ts-expect-error @typescript-eslint/ban-ts-comment
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
 });
